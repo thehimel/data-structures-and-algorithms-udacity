e92221eeec7716ecc51ac5fbf6c2f64269f43bf6
Himel Das <himelhub@gmail.com>
2020-05-26 22:22:51 +0600

    refactor

---
+++ m01c04-python/i03e02_generator.py
@@ -0,0 +1,22 @@
+# Definition of the generator to produce even numbers.
+def all_even():
+    n = 0
+    while True:
+        yield n
+        n += 2
+
+
+my_gen = all_even()
+
+# Generate the first 5 even numbers.
+for i in range(5):
+    print(next(my_gen))
+
+# Now go and do some other processing.
+do_something = 4
+do_something += 3
+print(do_something)
+
+# Now go back to generating more even numbers.
+for i in range(100):
+    print(next(my_gen))
