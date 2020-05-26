33ac5262ca3a78362deca8db4115bc13917c6117
Himel Das <himelhub@gmail.com>
2020-05-26 22:21:48 +0600

    refactor

---
+++ m01c04-python/i02e03_conditional_statements.py
@@ -0,0 +1,9 @@
+num = 5
+if num < 5:
+    print('The number is smaller than 5.')
+elif num == 5:
+    print('The number equals 5.')
+else:
+    print('The number is greater than 5.')
+
+# The code above would print: The number equals 5.
