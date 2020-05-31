31ed0b654dc7873c536872d3f163bd71743da097
Himel Das <himelhub@gmail.com>
2020-05-31 09:26:42 +0600

    refactor

--- m02c01-arrays-linked-lists/i14e00_duplicate_number.py
+++ m02c01-arrays-linked-lists/i14e00_duplicate_number.py
@@ -1,4 +1,18 @@
 """
+Problem Statement
+You have been given an array of length = n. The array contains integers
+from 0 to n - 2. Each number in the array is present exactly once
+except for one number which is present twice.
+Find and return this duplicate number present in the array
+
+Example:
+arr = [0, 2, 3, 1, 4, 5, 3]
+output = 3 (because 3 is present twice)
+The expected time complexity for this problem is O(n)
+and the expected space-complexity is O(1).
+"""
+
+"""
 Notice carefully that
 1. All the elements of the array are always non-negative
 2. If array length = n, then elements would start from 0 to (n-2),
