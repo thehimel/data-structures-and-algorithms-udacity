e9c3a3ef512bda93cbe1ae105110479b7f6f208e
Himel Das <himelhub@gmail.com>
2020-06-06 03:00:33 +0600

    refactor

---
+++ m02c03-recursion/i08e00_string_permutation.py
@@ -0,0 +1,41 @@
+"""
+Given an input string, find the list of strings that can be made
+with the permutation of the input string.
+input = 'abc'
+output = ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+"""
+"""
+Easy solution:
+TC: O(n!)
+SC: O(n!)
+"""
+
+
+def permute(string):
+    res = []
+    dfs(string, '', res)
+    return res
+
+
+def dfs(string, path, res):
+    print(f'\t--- dfs(string = {string}, path = {path}, res = {res})')
+
+    if not string:
+        res.append(path)
+
+    for i in range(len(string)):
+        dfs(string[:i] + string[i+1:], path+string[i], res)
+
+
+def test(input, expected_output):
+    output = permute(input)
+    print('Pass' if output == expected_output else 'Fail')
+
+
+input1 = 'ab'
+output1 = ['ab', 'ba']
+test(input1, output1)
+
+input2 = 'abc'
+output2 = ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+test(input2, output2)
