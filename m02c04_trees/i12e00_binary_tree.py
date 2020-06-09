72f93eae25108618ef0b3e2a6110f11aeb0c879c
Himel Das <himelhub@gmail.com>
2020-06-09 08:05:49 +0600

    refactor

---
+++ m02c04_trees/i12e00_binary_tree.py
@@ -0,0 +1,54 @@
+class Node(object):
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def set_left_child(self, node):
+        self.left = node
+
+    def set_right_child(self, node):
+        self.right = node
+
+    def get_left_child(self):
+        return self.left
+
+    def get_right_child(self):
+        return self.right
+
+    def has_left_child(self):
+        return self.left is not None
+
+    def has_right_child(self):
+        return self.right is not None
+
+
+# Binary Tree
+class Tree():
+    def __init__(self, value=None):
+        self.root = Node(value)
+
+    def get_root(self):
+        return self.root
+
+
+# Test Node
+node0 = Node("apple")
+node1 = Node("banana")
+node2 = Node("orange")
+
+print(f"has left child? {node0.has_left_child()}")
+print(f"has right child? {node0.has_right_child()}")
+
+print("adding left and right children")
+node0.set_left_child(node1)
+node0.set_right_child(node2)
+
+print(f"has left child? {node0.has_left_child()}")
+print(f"has right child? {node0.has_right_child()}")
