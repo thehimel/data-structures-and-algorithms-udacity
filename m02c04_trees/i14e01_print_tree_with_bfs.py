402076cf1c84206d9b44c8a1052d06e694d67acc
Himel Das <himelhub@gmail.com>
2020-06-09 13:56:48 +0600

    refactor

---
+++ m02c04_trees/i14e01_print_tree_with_bfs.py
@@ -0,0 +1,128 @@
+from collections import deque
+
+
+class Node(object):
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def set_left_child(self, left):
+        self.left = left
+
+    def set_right_child(self, right):
+        self.right = right
+
+    def get_left_child(self):
+        return self.left
+
+    def get_right_child(self):
+        return self.right
+
+    def has_left_child(self):
+        return self.left is not None
+
+    def has_right_child(self):
+        return self.right is not None
+
+    # define __repr_ to decide whata print statement
+    #   displays for a Node object
+    def __repr__(self):
+        return f"Node({self.get_value()})"
+
+    def __str__(self):
+        return f"Node({self.get_value()})"
+
+
+class Tree():
+    def __init__(self, value=None):
+        self.root = Node(value)
+
+    def get_root(self):
+        return self.root
+
+
+# create a tree and add some nodes
+tree = Tree("apple")
+tree.get_root().set_left_child(Node("banana"))
+tree.get_root().set_right_child(Node("cherry"))
+tree.get_root().get_left_child().set_left_child(Node("dates"))
+
+
+class Queue():
+    def __init__(self):
+        self.q = deque()
+
+    def enq(self, value):
+        self.q.appendleft(value)
+
+    def deq(self):
+        if len(self.q) > 0:
+            return self.q.pop()
+        else:
+            return None
+
+    def __len__(self):
+        return len(self.q)
+
+    def __repr__(self):
+        if len(self.q) > 0:
+            s = "<enqueue here>\n_________________\n"
+            s += "\n_________________\n".join([str(item) for item in self.q])
+            s += "\n_________________\n<dequeue here>"
+            return s
+        else:
+            return "<queue is empty>"
+
+
+# q = Queue()
+# q.enq("apple")
+# q.enq("banana")
+# q.enq("cherry")
+# print(q.deq())
+# print(q)
+
+
+def bfs(tree):
+    level = 0
+    q = Queue()
+    visit_order = list()
+    node = tree.get_root()
+    q.enq((node, level))
+    while(len(q) > 0):
+        node, level = q.deq()
+        if node is None:
+            visit_order.append(("<empty>", level))
+            continue
+        visit_order.append((node, level))
+        if node.has_left_child():
+            q.enq((node.get_left_child(), level+1))
+        else:
+            q.enq((None, level+1))
+
+        if node.has_right_child():
+            q.enq((node.get_right_child(), level+1))
+        else:
+            q.enq((None, level+1))
+
+    return visit_order
+
+
+visit_order = bfs(tree)
+s = "Tree\n"
+previous_level = -1
+for i in range(len(visit_order)):
+    node, level = visit_order[i]
+    if level == previous_level:
+        s += " | " + str(node)
+    else:
+        s += "\n" + str(node)
+        previous_level = level
+
+print(s)
