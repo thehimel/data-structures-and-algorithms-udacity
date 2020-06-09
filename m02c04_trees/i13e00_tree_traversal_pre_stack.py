59cebbceba870890fdbee258d38b7be0d49a2d04
Himel Das <himelhub@gmail.com>
2020-06-09 11:40:57 +0600

    refactor

---
+++ m02c04_trees/i13e00_tree_traversal_pre_stack.py
@@ -0,0 +1,167 @@
+"""
+Depth first, pre-order traversal
+
+Tree Traversal Logic
+Visit: V
+Left: L
+Right: R
+
+Pre-order traversal: VLR (V at the beginning)
+In-order traversal: LVR (V in the middle)
+Post-order traversal: LRV (V at last)
+"""
+
+
+# Let's define a stack to help keep track of the tree nodes
+class Stack():
+    def __init__(self):
+        self.list = list()
+
+    def push(self, value):
+        self.list.append(value)
+
+    def pop(self):
+        return self.list.pop()
+
+    def top(self):
+        if len(self.list) > 0:
+            return self.list[-1]
+        else:
+            return None
+
+    def is_empty(self):
+        return len(self.list) == 0
+
+    def __repr__(self):
+        if len(self.list) > 0:
+            s = "<top of stack>\n_________________\n"
+            s += "\n_________________\n".join([str(item) for item in self.list[::-1]])
+            s += "\n_________________\n<bottom of stack>"
+            return s
+
+        else:
+            return "<stack is empty>"
+
+
+# this code makes the tree that we'll traverse
+class Node(object):
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def set_left_child(self, left):
+        self.left = left
+
+    def set_right_child(self, right):
+        self.right = right
+
+    def get_left_child(self):
+        return self.left
+
+    def get_right_child(self):
+        return self.right
+
+    def has_left_child(self):
+        return self.left is not None
+
+    def has_right_child(self):
+        return self.right is not None
+
+    # define __repr_ to decide whata print statement
+    #   displays for a Node object
+    def __repr__(self):
+        return f"Node({self.get_value()})"
+
+    def __str__(self):
+        return f"Node({self.get_value()})"
+
+
+class Tree():
+    def __init__(self, value=None):
+        self.root = Node(value)
+
+    def get_root(self):
+        return self.root
+
+
+# create a tree and add some nodes
+tree = Tree("apple")
+tree.get_root().set_left_child(Node("banana"))
+tree.get_root().set_right_child(Node("cherry"))
+tree.get_root().get_left_child().set_left_child(Node("dates"))
+
+
+# Pre-order traversal using a stack, tracking state
+class State(object):
+    def __init__(self, node):
+        self.node = node
+        self.visited_left = False
+        self.visited_right = False
+
+    def get_node(self):
+        return self.node
+
+    def get_visited_left(self):
+        return self.visited_left
+
+    def get_visited_right(self):
+        return self.visited_right
+
+    def set_visited_left(self):
+        self.visited_left = True
+
+    def set_visited_right(self):
+        self.visited_right = True
+
+    def __repr__(self):
+        return f'{self.node}\nvisited_left: {self.visited_left}\nvisited_right: {self.visited_right}'
+
+
+def pre_order_with_stack(tree, debug_mode=False):
+    visit_order = list()
+    stack = Stack()
+    node = tree.get_root()
+    visit_order.append(node.get_value())
+    state = State(node)
+    stack.push(state)
+    count = 0
+    while(node):
+        if debug_mode:
+            print(f'loop count: {count}\ncurrent node: {node}\nstack: {stack}')
+        count += 1
+        if node.has_left_child() and not state.get_visited_left():
+            state.set_visited_left()
+            node = node.get_left_child()
+            visit_order.append(node.get_value())
+            state = State(node)
+            stack.push(state)
+
+        elif node.has_right_child() and not state.get_visited_right():
+            state.set_visited_right()
+            node = node.get_right_child()
+            visit_order.append(node.get_value())
+            state = State(node)
+
+        else:
+            stack.pop()
+            if not stack.is_empty():
+                state = stack.top()
+                node = state.get_node()
+            else:
+                node = None
+
+    if debug_mode:
+        print(f'loop count: {count}\ncurrent node: {node}\nstack: {stack}')
+
+    return visit_order
+
+
+# check pre-order traversal
+pre_order_with_stack(tree, debug_mode=True)
---
+++ m02c04_trees/i13e01_tree_traversal_dfs_recursion.py
@@ -0,0 +1,10 @@
+"""
+Tree Traversal Logic
+Visit: V
+Left: L
+Right: R
+
+Pre-order traversal: VLR (V at the beginning)
+In-order traversal: LVR (V in the middle)
+Post-order traversal: LRV (V at last)
+"""
