48bafc8acafd307dc3db0e349a7ed082bad88e01
Himel Das <himelhub@gmail.com>
2020-06-10 07:07:51 +0600

    refactor

---
+++ m02c05_maps and_hashing/i03e00_map_intro.py
@@ -0,0 +1,25 @@
+udacity = {}
+udacity['u'] = 1
+udacity['d'] = 2
+udacity['a'] = 3
+udacity['c'] = 4
+udacity['i'] = 5
+udacity['t'] = 6
+udacity['y'] = 7
+print(udacity)
+# {'u': 1, 'd': 2, 'a': 3, 'c': 4, 'i': 5, 't': 6, 'y': 7}
+
+dictionary = {}
+dictionary['d'] = [1]
+dictionary['i'] = [2]
+dictionary['c'] = [3]
+dictionary['t'] = [4]
+dictionary['i'].append(5)
+dictionary['o'] = [6]
+dictionary['n'] = [7]
+dictionary['a'] = [8]
+dictionary['r'] = [9]
+dictionary['y'] = [10]
+print(dictionary)
+# {'d': [1], 'i': [2, 5], 'c': [3], 't': [4], 'o': [6],
+#   'n': [7], 'a': [8], 'r': [9], 'y':[10]}
