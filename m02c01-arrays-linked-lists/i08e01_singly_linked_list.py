d00ecca0c469f1056548240346537f4da4d815d9
Himel Das <himelhub@gmail.com>
2020-06-13 11:25:51 +0600

    refactor

--- m02c01-arrays-linked-lists/i08e01_singly_linked_list.py
+++ m02c01-arrays-linked-lists/i08e01_singly_linked_list.py
@@ -22,14 +22,46 @@ class LinkedList:
         node.next = Node(value)
         return
 
+    def remove(self, value):
+        if self.head is None:
+            return None
+
+        if self.head.value == value:
+            self.head = self.head.next
+            return value
+
+        previous = self.head
+        current = previous.next
+
+        while(current):
+            if current.value == value:
+                previous.next = current.next
+                return value
+            previous = current
+            current = current.next
+
+    def __str__(self):
+        node = self.head
+        out_string = ""
+        while node:
+            out_string += str(node.value) + " -> "
+            node = node.next
+        out_string += 'None'  # Indicate the end of linked list
+        return out_string
+
 
 # 1 -> 2 -> 4
 linked_list = LinkedList()
 linked_list.append(1)
 linked_list.append(2)
 linked_list.append(4)
+print(linked_list)
 
-node = linked_list.head
-while node:
-    print(node.value)
-    node = node.next
+print(linked_list.remove(10))
+print(linked_list)
+
+print(linked_list.remove(4))
+print(linked_list)
+
+linked_list.append(5)
+print(linked_list)
--- p02-show-me-the-data-structures/t06_union_and_Intersection.py
+++ p02-show-me-the-data-structures/t06_union_and_Intersection.py
@@ -11,47 +11,89 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+        self.size = 0
 
     def append(self, value):
         new_node = Node(value)
+
         if self.head is None:
             self.head = new_node
             self.tail = self.head
+            self.size += 1
             return
 
         self.tail.next = new_node
         self.tail = new_node
+        self.size += 1
 
-    def size(self):
-        size = 0
-        node = self.head
-        while node:
-            size += 1
-            node = node.next
+    def remove(self, value):
+        if self.head is None:
+            return
 
-        return size
+        if self.head.value == value:
+            self.head = self.head.next
+
+        previous = self.head
+        current = previous.next
+
+        while(current):
+            if current.value == value:
+                previous.next = current.next
+                return
+            previous = current
+            current = current.next
+
+    def get_size(self):
+        return self.size
 
     def __str__(self):
-        cur_head = self.head
+        node = self.head
         out_string = ""
-        while cur_head:
-            out_string += str(cur_head.value) + " -> "
-            cur_head = cur_head.next
+        while node:
+            out_string += str(node.value) + " -> "
+            node = node.next
+        out_string += 'None'  # Indicate the end of linked list
         return out_string
 
 
 def union(llist_1, llist_2):
-    # Your Solution Here
-    pass
+    if llist_1.get_size() == 0:
+        return llist_2
+
+    if llist_2.get_size() == 0:
+        return llist_1
+
+    llist = llist_1
+    llist.tail.next = llist_2.head
+
+    return llist
+
+
+def is_present(llist, search_node):
+    node = llist.head
+    while node:
+        if node.value == search_node.value:
+            return True
+        node = node.next
+
+    return False
 
 
 def intersection(llist_1, llist_2):
-    # Your Solution Here
-    pass
+    llist = LinkedList()
+    if llist_1.get_size() == 0 or llist_2.get_size() == 0:
+        return llist  # Return an empty linked list
+
+    node = llist_2.head
+    while node:
+        if is_present(llist_1, node):
+            llist.append(node.value)
+        node = node.next
+
+    return llist
 
 
 # Test case 1
-
 linked_list_1 = LinkedList()
 linked_list_2 = LinkedList()
 
@@ -64,11 +106,13 @@ for i in element_1:
 for i in element_2:
     linked_list_2.append(i)
 
+print(linked_list_1)
+print(linked_list_2)
+
 print(union(linked_list_1, linked_list_2))
 print(intersection(linked_list_1, linked_list_2))
 
 # Test case 2
-
 linked_list_3 = LinkedList()
 linked_list_4 = LinkedList()
 
