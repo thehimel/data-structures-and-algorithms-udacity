872ac19e5eb8116fd491b62e1a0b640d6ceeede4
Himel Das <himelhub@gmail.com>
2020-06-13 18:15:28 +0600

    refactor

--- p02-show-me-the-data-structures/t06_union_and_Intersection.py
+++ p02-show-me-the-data-structures/t06_union_and_Intersection.py
@@ -1,7 +1,8 @@
 """
-Complexity Alalysis:
+Complexity Analysis:
 Operation 1: Union
 ..................
+Note: All elements in the union must be unique.
 Union = linked_list_1 + linked_list_2
 M = size of the first linked list
 N = size of the second linked list
@@ -15,15 +16,12 @@ SC: O(n)
 
 Operation 2: Intersection
 .........................
-Intersection = Common nodes between linked_list_1 and linked_list_2
+Intersection = Unique common nodes between linked_list_1 and linked_list_2.
+We create a set of linked_list_1 so that the lookup can be done with O(1).
 For every node in linked_list_2, we check if the value is presented in
-the linked_list_1 or not.
+the set of linked_list_1 or not.
 
-TC: O(M*N)
-SC: O(n)
-
-This can also be written as:
-TC: O(n^2)
+TC: O(n)
 SC: O(n)
 """
 
@@ -69,10 +67,16 @@ class LinkedList:
         return out_string
 
 
+# Copy only the unique elements. Thus set is used to O(1) lookup.
 def copy_linked_list(llist, new_llist):
+    llist_set = get_llist_set(llist)
+
     node = new_llist.head
     while(node):
-        llist.append(node.value)
+        value = node.value
+        if value not in llist_set:
+            llist.append(value)
+            llist_set.add(value)
         node = node.next
 
 
@@ -102,17 +106,15 @@ def union(llist_1, llist_2):
     return llist
 
 
-def is_present(llist, search_node):
-    if llist.get_size() == 0:
-        return False
+def get_llist_set(llist):
+    llist_set = set()
 
     node = llist.head
     while node:
-        if node.value == search_node.value:
-            return True
+        llist_set.add(node.value)
         node = node.next
 
-    return False
+    return llist_set
 
 
 def intersection(llist_1, llist_2):
@@ -120,9 +122,12 @@ def intersection(llist_1, llist_2):
     if llist_1.get_size() == 0 or llist_2.get_size() == 0:
         return llist  # Return an empty linked list
 
+    # Set allows O(1) lookup
+    llist_set = get_llist_set(llist_1)
+
     node = llist_2.head
     while node:
-        if is_present(llist_1, node):
+        if node.value in llist_set:
             llist.append(node.value)
         node = node.next
 
@@ -130,8 +135,8 @@ def intersection(llist_1, llist_2):
 
 
 def test(linked_list_1, linked_list_2):
-    print(linked_list_1)
-    print(linked_list_2)
+    print(f'Set 1: {linked_list_1}')
+    print(f'Set 2: {linked_list_2}')
     print(f'Union: {union(linked_list_1, linked_list_2)}')
     print(f'Intersection: {intersection(linked_list_1, linked_list_2)}')
     print()
@@ -177,3 +182,18 @@ for i in element:
     linked_list_6.append(i)
 
 test(linked_list_5, linked_list_6)
+
+
+# Test case 4
+linked_list_7 = LinkedList()
+linked_list_8 = LinkedList()
+element7 = [1, 2, 3, 4, 5, 6, 7]
+element8 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for i in element7:
+    linked_list_7.append(i)
+
+for i in element8:
+    linked_list_8.append(i)
+
+test(linked_list_7, linked_list_8)
