1118b17f4ef34bd129dcaef53a5f92c619698290
Himel Das <himelhub@gmail.com>
2020-06-13 18:32:23 +0600

    refactor

--- p02-show-me-the-data-structures/t01_lru_cache.py
+++ p02-show-me-the-data-structures/t01_lru_cache.py
@@ -38,10 +38,13 @@ During the removal of the oldest key, we are just performing the pop operation.
 """
 
 
+import collections
+
+
 class LRU_Cache(object):
     # Initialize class variables
     def __init__(self, capacity):
-        self.cache = {}
+        self.cache = collections.OrderedDict()
         self.cache_size = 0
         self.max_cache_size = 5
         self.lru_key = None
@@ -49,6 +52,7 @@ class LRU_Cache(object):
     # Retrieve item from provided key. Return -1 if nonexistent.
     def get(self, key):
         if key in self.cache:
+            self.lru_key = self.cache[key]
             return self.cache[key]
         return -1
 
@@ -72,6 +76,17 @@ class LRU_Cache(object):
             self.lru_key = key
             # print(f'size = {self.cache_size}')
 
+    def __str__(self):
+        out_string = ''
+        for key in self.cache:
+            out_string += str(key) + '->'
+        out_string += 'End'
+
+        return out_string
+
+    def get_lru_key(self):
+        return self.lru_key
+
 
 our_cache = LRU_Cache(5)
 
@@ -80,17 +95,29 @@ our_cache.set(2, 2)
 our_cache.set(3, 3)
 our_cache.set(4, 4)
 
+print(our_cache)
+print(f'LRU Key: {our_cache.get_lru_key()}')
 
 print(our_cache.get(1))  # returns 1
+print(f'LRU Key: {our_cache.get_lru_key()}')
+
 print(our_cache.get(2))  # returns 2
+print(f'LRU Key: {our_cache.get_lru_key()}')
+
 print(our_cache.get(9))  # returns -1 because 9 is not present in the cache
+print(f'LRU Key: {our_cache.get_lru_key()}')
 
 our_cache.set(5, 5)
 print(our_cache.get(5))
+print(f'LRU Key: {our_cache.get_lru_key()}')
 
 our_cache.set(6, 6)
 
 # returns -1 because the cache reached it's capacity
 # and 5 was the least recently used entry
 print(our_cache.get(5))
+print(f'LRU Key: {our_cache.get_lru_key()}')
+
 print(our_cache.get(6))
+print(our_cache)
+print(f'LRU Key: {our_cache.get_lru_key()}')
