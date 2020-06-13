6ae2a470548a18226039999aa34d22663f5b83c4
Himel Das <himelhub@gmail.com>
2020-06-13 14:04:34 +0600

    refactor

---
+++ m03c01-basic-algorithms/i01e01_binary_search_recursive.py
@@ -0,0 +1,53 @@
+"""
+Complexity:
+At every step, the input size is being half.
+TC: O(log(n))
+SC: O(1)
+"""
+
+
+# Recursive solution
+def search(array, target, start, end):
+    if start > end:
+        return -1
+
+    mid = (start + end)//2
+    mid_element = array[mid]
+
+    if mid_element == target:
+        return mid
+
+    elif target < mid_element:
+        end = mid - 1
+        return search(array, target, start, end)
+
+    else:
+        start = mid + 1
+        return search(array, target, start, end)
+
+
+def binary_search(array, target):
+    start = 0
+    end = len(array) - 1
+    return search(array, target, start, end)
+
+
+def test_function(test_case):
+    answer = binary_search(test_case[0], test_case[1])
+    if answer == test_case[2]:
+        print("Pass!")
+    else:
+        print("Fail!")
+
+
+array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+target = 4
+index = 4
+test_case = [array, target, index]
+test_function(test_case)
+
+array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+target = 100
+index = -1
+test_case = [array, target, index]
+test_function(test_case)
