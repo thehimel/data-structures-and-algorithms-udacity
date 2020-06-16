133c0779835acf0d91d7b16c8c2bc68b0d4a9367
Himel Das <himelhub@gmail.com>
2020-06-16 11:07:29 +0600

    refactor

--- m02c01-arrays-linked-lists/i15e01_max_sum_subarray.py
+++ m02c01-arrays-linked-lists/i15e01_max_sum_subarray.py
@@ -19,46 +19,40 @@ def max_sum_subarray(arr):
     current_sum = arr[0]
     max_sum = arr[0]
 
-    current_sum_sub_array = [arr[0]]
+    current_sum_sub_arr = [arr[0]]
     max_sum_sub_arr = [arr[0]]
 
     # Traverse from index 1 to the end
     for i in range(1, len(arr)):
         element = arr[i]
+
         if current_sum + element > element:
-            current_sum_sub_array = current_sum_sub_array + [element]
+            current_sum_sub_arr = current_sum_sub_arr + [element]
+            current_sum += element
         else:
-            current_sum_sub_array = [element]
+            current_sum_sub_arr = [element]
+            current_sum = element
 
         if current_sum > max_sum:
-            max_sum_sub_arr = current_sum_sub_array
+            max_sum_sub_arr = current_sum_sub_arr
             max_sum = current_sum
 
     return max_sum, max_sum_sub_arr
 
 
-def test_function(test_case):
-    arr = test_case[0]
-    solution = test_case[1]
-
-    output = max_sum_subarray(arr)
-    if output == solution:
-        print("Pass")
-    else:
-        print("Fail")
+def test(input, output):
+    max_sum, max_sum_sub_arr = max_sum_subarray(input)
+    print('Pass' if output == max_sum_sub_arr else 'Fail')
 
 
-arr = [1, 2, 3, -4, 6]
-solution = 8, [1, 2, 3, -4, 6]  # sum of array
-test_case = [arr, solution]
-test_function(test_case)
+input = [1, 2, 3, -4, 6]
+output = [1, 2, 3, -4, 6]
+test(input, output)
 
-arr = [1, 2, -5, -4, 1, 6]
-solution = 7   # sum of last two elements
-test_case = [arr, solution]
-test_function(test_case)
+input = [1, 2, -5, -4, 1, 6]
+output = [1, 6]
+test(input, output)
 
-arr = [-12, 15, -13, 14, -1, 2, 1, -5, 4]
-solution = 18  # sum of subarray = [15, -13, 14, -1, 2, 1]
-test_case = [arr, solution]
-test_function(test_case)
+input = [-12, 15, -13, 14, -1, 2, 1, -5, 4]
+output = [15, -13, 14, -1, 2, 1]
+test(input, output)
