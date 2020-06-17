266ce38a03aaec71adc56a7fea0733600d762f56
Himel Das <himelhub@gmail.com>
2020-06-17 18:42:29 +0600

    refactor

--- m04c01-greedy-algorithms/i03e00_min_platforms.py
+++ m04c01-greedy-algorithms/i03e00_min_platforms.py
@@ -18,7 +18,81 @@ departure = [910, 1200, 1120, 1130, 1900, 2000]
 Expected output: Minimum number of platforms required = 3
 
 The greedy approach:
-Sort the schedule, and make sure when a train arrives or depart, keep track of the required number of platforms. We will have iterator i and j traversing the arrival and departure lists respectively. At any moment, the difference (i - j) will provide us the required number of platforms.
+Sort the schedule, and make sure when a train arrives or depart, keep track of
+the required number of platforms. We will have iterator i and j traversing the
+arrival and departure lists respectively. At any moment, the difference (i - j)
+will provide us the required number of platforms.
 
-At the time of either arrival or departure of a train, if i^th arrival is scheduled before the j^th departure, increment the platform_required and i as well. Otherwise, decrement platform_required count, and increase j. Keep track of the max value of platform_required ever, as the expected result.
+At the time of either arrival or departure of a train, if i^th arrival is
+scheduled before the j^th departure, increment the platform_required and i.
+Otherwise, decrement platform_required count, and increase j.
+Keep track of the max value of platform_required ever, as the expected result.
+
+Complexity Analysis:
+TC: O(n) + T(Python Sort: Tim Sort) => O(n) + O(nlogn) => O(nlogn)
+TC: O(nlogn)
 """
+
+
+def min_platforms(arrival, departure):
+    # Mismatch in the length of the lists
+    if(len(arrival) != len(departure)):
+        return -1
+
+    # Sort both the lists.
+    arrival.sort()
+    departure.sort()
+
+    # Count of platforms required when comparing i^th arrival
+    # and j^th departure
+    platform_required = 1
+
+    # Keep track of the max value of platform_required
+    max_platform_required = 1
+
+    # Iterate such that (i-j) will represent platform_required at that moment
+    i = 1
+    j = 0
+
+    # Traverse the arrival list with iterator `i`, and departure with `j`
+    while i < len(arrival) and j < len(arrival):
+
+        # if i^th arrival is scheduled before than j^th departure,
+        # increment platform_required and i as well.
+        if arrival[i] < departure[j]:
+            platform_required += 1
+            i += 1
+
+            # Update the max value of platform_required
+            if platform_required > max_platform_required:
+                max_platform_required = platform_required
+
+        # Otherwise, decrement platform_required count, and increase j.
+        else:
+            platform_required -= 1
+            j += 1
+
+    return max_platform_required
+
+
+def test_function(test_case):
+    arrival = test_case[0]
+    departure = test_case[1]
+    solution = test_case[2]
+
+    output = min_platforms(arrival, departure)
+    if output == solution:
+        print("Pass")
+    else:
+        print("Fail")
+
+
+arrival = [900,  940, 950,  1100, 1500, 1800]
+departure = [910, 1200, 1120, 1130, 1900, 2000]
+test_case = [arrival, departure, 3]
+
+test_function(test_case)
+arrival = [200, 210, 300, 320, 350, 500]
+departure = [230, 340, 320, 430, 400, 520]
+test_case = [arrival, departure, 2]
+test_function(test_case)
