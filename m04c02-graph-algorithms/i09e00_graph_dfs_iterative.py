9a72bab3d35f2f81e7e80a9a433d4c850887c223
Himel Das <himelhub@gmail.com>
2020-06-18 10:06:46 +0600

    refactor

--- m04c02-graph-algorithms/i09e00_graph_dfs_iterative.py
+++ m04c02-graph-algorithms/i09e00_graph_dfs_iterative.py
@@ -11,8 +11,8 @@ starting at the root_node.
 
 
 class GraphNode(object):
-    def __init__(self, val):
-        self.value = val
+    def __init__(self, value):
+        self.value = value
         self.children = []
 
     def add_child(self, new_node):
@@ -38,23 +38,6 @@ class Graph(object):
             node2.remove_child(node1)
 
 
-nodeG = GraphNode('G')
-nodeR = GraphNode('R')
-nodeA = GraphNode('A')
-nodeP = GraphNode('P')
-nodeH = GraphNode('H')
-nodeS = GraphNode('S')
-
-graph1 = Graph([nodeS, nodeH, nodeG, nodeP, nodeR, nodeA])
-graph1.add_edge(nodeG, nodeR)
-graph1.add_edge(nodeA, nodeR)
-graph1.add_edge(nodeA, nodeG)
-graph1.add_edge(nodeR, nodeP)
-graph1.add_edge(nodeH, nodeG)
-graph1.add_edge(nodeH, nodeP)
-graph1.add_edge(nodeS, nodeR)
-
-
 # Solution
 def dfs_search(root_node, target):
     visited = set()                         # Sets are faster for lookups
@@ -76,6 +59,23 @@ def dfs_search(root_node, target):
             if (child not in visited) and (child not in stack):
                 stack.append(child)
 
+
+nodeG = GraphNode('G')
+nodeR = GraphNode('R')
+nodeA = GraphNode('A')
+nodeP = GraphNode('P')
+nodeH = GraphNode('H')
+nodeS = GraphNode('S')
+
+node_list = [nodeS, nodeH, nodeG, nodeP, nodeR, nodeA]
+graph1 = Graph(node_list)
+graph1.add_edge(nodeG, nodeR)
+graph1.add_edge(nodeA, nodeR)
+graph1.add_edge(nodeA, nodeG)
+graph1.add_edge(nodeR, nodeP)
+graph1.add_edge(nodeH, nodeG)
+graph1.add_edge(nodeH, nodeP)
+graph1.add_edge(nodeS, nodeR)
 
 # Tests
 assert nodeA == dfs_search(nodeS, 'A')
