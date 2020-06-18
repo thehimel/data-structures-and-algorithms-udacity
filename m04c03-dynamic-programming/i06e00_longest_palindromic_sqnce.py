0633dfbee746e349826384357f0a785d29b79af0
Himel Das <himelhub@gmail.com>
2020-06-18 21:23:16 +0600

    refactor

--- m04c03-dynamic-programming/i06e00_longest_palindromic_sqnce.py
+++ m04c03-dynamic-programming/i06e00_longest_palindromic_sqnce.py
@@ -36,30 +36,32 @@ import pprint
 pp = pprint.PrettyPrinter()
 
 
-# complete LPS solution
+# Complete LPS solution
 def lps(input_string):
     n = len(input_string)
 
-    # create a lookup table to store results of subproblems
+    # Create a lookup table to store results of subproblems
     L = [[0 for x in range(n)] for x in range(n)]
 
-    # strings of length 1 have LPS length = 1
+    # Strings of length 1 have LPS length = 1
     for i in range(n):
         L[i][i] = 1
 
-    # consider all substrings
+    # Consider all substrings
     for s_size in range(2, n+1):
         for start_idx in range(n-s_size+1):
             end_idx = start_idx + s_size - 1
             if s_size == 2 and input_string[start_idx] == input_string[end_idx]:
-                # match with a substring of length 2
+                # Match with a substring of length 2
                 L[start_idx][end_idx] = 2
             elif input_string[start_idx] == input_string[end_idx]:
-                # general match case
+                # General match case
                 L[start_idx][end_idx] = L[start_idx+1][end_idx-1] + 2
             else:
-                # no match case, taking the max of two values
-                L[start_idx][end_idx] = max(L[start_idx][end_idx-1], L[start_idx+1][end_idx]);
+                # No match case, taking the max of two values
+                x = L[start_idx][end_idx-1]
+                y = L[start_idx+1][end_idx]
+                L[start_idx][end_idx] = max(x, y)
 
     # debug line
     # pp.pprint(L)
