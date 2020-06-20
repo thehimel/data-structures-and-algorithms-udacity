e144f06846e3a51699da73afb316e2a30c02c820
Himel Das <himelhub@gmail.com>
2020-06-20 09:06:10 +0600

    refactor

--- p03-problems-vs-algorithns/t02_search_in_rotated_sorted_array.py
+++ p03-problems-vs-algorithns/t02_search_in_rotated_sorted_array.py
@@ -2,7 +2,7 @@
 Task 2: Search in a Rotated Sorted Array
 
 You are given a sorted array which is rotated at some random pivot point.
-Example: [0, 1, 2, 4, 5, 6, 7] might become [4, 5, 6, 7, 0, 1, 2]
+Example: [0, 1, 2, 4, 5, 6, 7] might become [4, 5, 6, 7, 0, 1, 2].
 
 You are given a target value to search. If found in the array return its index,
 otherwise return -1. You can assume there are no duplicates in the array and
