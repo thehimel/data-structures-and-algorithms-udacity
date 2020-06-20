51bd4fad99c7354757690232e99c7b3f2050bb13
Himel Das <himelhub@gmail.com>
2020-06-20 09:03:57 +0600

    refactor

--- p02-show-me-the-data-structures/t06_union_and_Intersection.py
+++ p02-show-me-the-data-structures/t06_union_and_Intersection.py
@@ -1,4 +1,13 @@
 """
+Union and Intersection of Two Linked Lists
+Implement the union and intersection functions. The union of two sets A and B,
+A ∪ B is the set of elements which are in A, in B, or in both A and B.
+The intersection of two sets A and B, denoted by A ∩ B, is the set of all
+elements that are members of both the sets A and B.
+
+You will take in two linked lists and return a linked list that is composed of
+either the union or intersection, respectively.
+
 Complexity Analysis:
 Operation 1: Union
 ..................
