1749f48ae12e09043a7096d2f13b9d03295d385e
Himel Das <himelhub@gmail.com>
2020-06-20 08:57:01 +0600

    refactor

--- p02-show-me-the-data-structures/t05_blockchain.py
+++ p02-show-me-the-data-structures/t05_blockchain.py
@@ -1,4 +1,19 @@
 """
+Blockchain
+
+A Blockchain is a sequential chain of records, similar to a linked list.
+Each block contains some information and how it is connected related to
+the other blocks in the chain. Each block contains a cryptographic hash
+of the previous block, a timestamp, and transaction data.
+For our blockchain we will be using a SHA-256 hash, the Greenwich Mean Time
+(GMT) when the block was created, and text strings as the data.
+
+Use your knowledge of linked lists and hashing to create
+a blockchain implementation.
+
+
+Solution
+
 Every block has some data: data, timestamp, hash, and previous hash.
 
 Every block node consists a block and a next pointer to the next block.
