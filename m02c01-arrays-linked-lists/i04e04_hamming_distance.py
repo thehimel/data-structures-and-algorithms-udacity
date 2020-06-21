0bc951f3721b5fb7f1fa482defce4b7f9df56c03
Himel Das <himelhub@gmail.com>
2020-06-21 13:49:29 +0600

    refactor

--- m02c01-arrays-linked-lists/i04e04_hamming_distance.py
+++ m02c01-arrays-linked-lists/i04e04_hamming_distance.py
@@ -15,20 +15,25 @@ Returns:
 
 
 def hamming_distance(str1, str2):
-    if len(str1) == len(str2):
-        count = 0
+    if len(str1) != len(str2):
+        return None
 
-        for char in range(len(str1)):
-            if str1[char] != str2[char]:
-                count += 1
+    count = 0
+    for char in range(len(str1)):
+        if str1[char] != str2[char]:
+            count += 1
 
-        return count
-
-    return None
+    return count
 
 
-print("Pass" if (10 == hamming_distance('ACTTGACCGGG', 'GATCCGGTACA')) else "Fail")
-print("Pass" if (1 == hamming_distance('shove', 'stove')) else "Fail")
-print("Pass" if (None is hamming_distance('Slot machines', 'Cash lost in me')) else "Fail")
-print("Pass" if (9 == hamming_distance('A gentleman', 'Elegant men')) else "Fail")
-print("Pass" if (2 == hamming_distance('0101010100011101', '0101010100010001')) else "Fail")
+def test(str1, str2, output):
+    print("Pass" if hamming_distance(str1, str2) == output else "Fail")
+
+
+test('0000', '1111', 4)
+test('0101', '0000', 2)
+test('ACTTGACCGGG', 'GATCCGGTACA', 10)
+test('shove', 'stove', 1)
+test('Slot machines', 'Cash lost in me', None)
+test('A gentleman', 'Elegant men', 9)
+test('0101010100011101', '0101010100010001', 2)
