2efef0d412dad77617db330146800afc701c8d2c
Himel Das <himelhub@gmail.com>
2020-06-21 13:28:55 +0600

    refactor

--- m02c01-arrays-linked-lists/i04e01_reverse_string.py
+++ m02c01-arrays-linked-lists/i04e01_reverse_string.py
@@ -1,31 +1,37 @@
 # Exercise 1. Reverse Strings
-def string_reverser(our_string):
-
-    """
-    Reverse the input string
-
-    Args:
-       our_string(string): String to be reversed
-    Returns:
-       string: The reversed string
-    """
-
+def string_reverser(string):
     # New empty string for us to build on
     new_string = ""
 
-    # Iterate over old string
-    for i in range(len(our_string)):
-        # Grab the charecter from the back of the string and add them to the new string
-        new_string += our_string[(len(our_string)-1)-i]
+    length = len(string)
+    last_index = length-1
+
+    # Iterate over old string. Grab the charecter from the back of the string
+    # and add them to the new string
+    for i in range(length):
+        new_string += string[last_index-i]
 
     # Return our solution
     return new_string
 
 
-# Test Cases
-print("Pass" if ('retaw' == string_reverser('water')) else "Fail")
-print("Pass" if ('!noitalupinam gnirts gnicitcarP' == string_reverser('Practicing string manipulation!')) else "Fail")
-print("Pass" if ('3432 :si edoc esuoh ehT' == string_reverser('The house code is: 2343')) else "Fail")
+# Test
+def test(function, input, output):
+    print("Pass" if output == function(input) else "Fail")
+
+
+input = "retaw"
+output = "water"
+test(string_reverser, input, output)
+
+input = "!noitalupinam gnirts gnicitcarP"
+output = "Practicing string manipulation!"
+test(string_reverser, input, output)
+
+input = "3432 :si edoc esuoh ehT"
+output = "The house code is: 2343"
+test(string_reverser, input, output)
+
 
 # Reverse string shortcut in Python
 """
@@ -39,6 +45,14 @@ def reverse_string(str):
     return str[::-1]
 
 
-print("Pass" if ('retaw' == reverse_string('water')) else "Fail")
-print("Pass" if ('!noitalupinam gnirts gnicitcarP' == reverse_string('Practicing string manipulation!')) else "Fail")
-print("Pass" if ('3432 :si edoc esuoh ehT' == reverse_string('The house code is: 2343')) else "Fail")
+input = "retaw"
+output = "water"
+test(reverse_string, input, output)
+
+input = "!noitalupinam gnirts gnicitcarP"
+output = "Practicing string manipulation!"
+test(reverse_string, input, output)
+
+input = "3432 :si edoc esuoh ehT"
+output = "The house code is: 2343"
+test(reverse_string, input, output)
--- m02c01-arrays-linked-lists/i04e02_anagrams.py
+++ m02c01-arrays-linked-lists/i04e02_anagrams.py
@@ -1,15 +1,18 @@
 """
 Exercise 2. Anagrams
-The goal of this exercise is to write some code to determine if two strings are anagrams of each other.
+The goal of this exercise is to write some code to determine if two strings
+are anagrams of each other.
 
-An anagram is a word (or phrase) that is formed by rearranging the letters of another word (or phrase).
+An anagram is a word (or phrase) that is formed by rearranging the letters of
+another word (or phrase).
 
 For example:
 
 "rat" is an anagram of "art"
 "alert" is an anagram of "alter"
 "Slot machines" is an anagram of "Cash lost in me"
-Your function should take two strings as input and return True if the two words are anagrams and False if they are not.
+Your function should take two strings as input and return True if the two
+words are anagrams and False if they are not.
 
 You can assume the following about the input strings:
 
@@ -17,20 +20,24 @@ No punctuation
 No numbers
 No special characters
 Note - You can use built-in methods len(), lower() and sort() on strings.
+
+Check if the input strings are anagrams
+
+Args:
+    str1(string),str2(string): Strings to be checked if they are anagrams
+Returns:
+    bool: If strings are anagrams or not
+
+Solution:
+Remove the spaces from both the strings.
+Sort the strings and match them with each other.
+
+TC: O(n)
+SC: O(n)
 """
 
 
 def anagram_checker(str1, str2):
-
-    """
-    Check if the input strings are anagrams
-
-    Args:
-       str1(string),str2(string): Strings to be checked if they are anagrams
-    Returns:
-       bool: If strings are anagrams or not
-    """
-
     # Clean strings
     clean_str_1 = str1.replace(" ", "").lower()
     clean_str_2 = str2.replace(" ", "").lower()
@@ -41,11 +48,15 @@ def anagram_checker(str1, str2):
     return False
 
 
-print("Pass" if (anagram_checker('rat', 'art')) else "Fail")
-print("Pass" if (anagram_checker('alert', 'alter')) else "Fail")
-print("Pass" if anagram_checker('Slot machines', 'Cash lost in me') else "Fail")
-print("Pass" if anagram_checker('Dormitory', 'Dirty room') else "Fail")
-print("Pass" if anagram_checker('Time and tide wait for no man', 'Notified madman into water') else "Fail")
+def test(str1, str2, output):
+    print("Pass" if output == anagram_checker(str1, str2) else "Fail")
 
-print("Pass" if not (anagram_checker('water', 'waiter')) else "Fail")
-print("Pass" if not (anagram_checker('A gentleman', 'Elegant men')) else "Fail")
+
+test('rat', 'art', True)
+test('alert', 'alter', True)
+test('Slot machines', 'Cash lost in me', True)
+test('Dormitory', 'Dirty room', True)
+test('Time and tide wait for no man', 'Notified madman into water', True)
+
+test('water', 'waiter', False)
+test('A gentleman', 'Elegant men', False)
