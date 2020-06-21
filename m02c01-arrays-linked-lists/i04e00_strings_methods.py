ae1be7fc3a966be6a20e48de0d4aeae4e413c28d
Himel Das <himelhub@gmail.com>
2020-06-21 11:28:24 +0600

    refactor

--- m02c01-arrays-linked-lists/i04e00_strings_methods.py
+++ m02c01-arrays-linked-lists/i04e00_strings_methods.py
@@ -9,7 +9,8 @@ print(len(str1))		# 7
 
 
 # CHANGE CASE
-# The `lower()` and `upper` method returns the string in lower case and upper case respectively
+# The `lower()` and `upper` method returns the string in lower case
+# and upper case respectively
 print(str1.lower())		# udacity
 print(str1.upper())		# UDACITY
 
@@ -32,12 +33,14 @@ print(str2.strip())		# Udacity
 
 
 # REPLACE/SUBSTITUTE A CHARACTER IN THE STRING
-# The replace() method replaces all occurances a character in a string with another character. The input arguments are case-sensitive
-print(str1.replace('y', "B")) #UdacitB
+# The replace() method replaces all occurances a character in a string
+# with another character. The input arguments are case-sensitive
+print(str1.replace('y', "B"))  # UdacitB
 
 
 # SPLIT INTO SUB-STRINGS
-# The split() method splits a string into substrings based on the separator that we specify as argument
+# The split() method splits a string into substrings based on the separator
+# that we specify as argument
 str3 = "Welcome, Constance!"
 print(str3.split(","))  # ['Welcome', ' Constance!']
 
@@ -45,10 +48,17 @@ print(str3.split(","))  # ['Welcome', ' Constance!']
 # CONCATENATION
 print(str3 + " " + str1)  # Welcome, Constance! Udacity
 marks = 100
-# print(str3 + " You have scored a perfect " + marks) # TypeError: can only concatenate str (not "int") to str
-print(str3 + " You have scored a perfect " + format(marks))  # format() method converts the argument as a formatted string
+# print(str3 + " You have scored a perfect " + marks)
+# TypeError: can only concatenate str (not "int") to str
+
+
+# format() method converts the argument as a formatted string
+print(str3 + " You have scored a perfect " + format(marks))
 
 
 # SORT A STRING
-# We can use sorted() method that sort any instance of *iterable*. The characters are compared based on their ascii value
-print(sorted(str3))  # [' ', '!', ',', 'C', 'W', 'a', 'c', 'c', 'e', 'e', 'e', 'l', 'm', 'n', 'n', 'o', 'o', 's', 't']
+# We can use sorted() method that sort any instance of *iterable*.
+# The characters are compared based on their ascii value
+# [' ', '!', ',', 'C', 'W', 'a', 'c', 'c', 'e', 'e', 'e',
+#   'l', 'm', 'n', 'n', 'o', 'o', 's', 't']
+print(sorted(str3))
